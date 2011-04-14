7e0d6b5541c69c0be54a840aaa4b066cd89e18e1
Karol Urbanski <d3xtero@gmail.com>
2011-04-14 13:05:32 +0200

    ostateczna wersja, mierzaca czasy

--- lab3/gsl_integration.c
+++ lab3/gsl_integration.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 #include <math.h>
 #include <gsl/gsl_integration.h>
+#include <sys/time.h>
+
+double get_time(void)
+{
+	struct timeval tv;
+	gettimeofday(&tv,NULL);
+	return (double)tv.tv_sec+(double)tv.tv_usec/1000000.0;
+}
 
 double f (double x, void * params) {
 	double f = log(x*x)+log(x);
@@ -30,12 +38,14 @@ void integrate_mine(double expected, double abserror)
 {
 	double result;
 	int intervals;
+	double time = get_time();
 	printf("Wlasna metoda prostokatow (blad %g): \n",abserror);
 	result = square_integrate(f,0,2,expected,abserror,&intervals);
 	printf ("result          = % .18f\n", result);
 	printf ("exact result    = % .18f\n", expected);
 	printf ("actual error    = % .18f\n", result - expected);
 	printf ("intervals       =  %d\n", intervals);
+	printf ("time            =  %f\n", get_time() - time);
 }
 
 void integrate_gsl_qags(double expected, double abserror)
@@ -44,6 +54,7 @@ void integrate_gsl_qags(double expected, double abserror)
 	double result, error;
 	double alpha = 1.0;
 	gsl_function F;
+	double time = get_time();
 	printf("QAGS GSL (blad %g): \n",abserror);
 	F.function = &f;
 	F.params = &alpha;
@@ -53,6 +64,7 @@ void integrate_gsl_qags(double expected, double abserror)
 	printf ("estimated error = % .18f\n", error);
 	printf ("actual error    = % .18f\n", result - expected);
 	printf ("intervals       =  %d\n", w->size);
+	printf ("time            =  %f\n", get_time() - time);
 	gsl_integration_workspace_free (w);
 }
 
@@ -62,6 +74,7 @@ void integrate_gsl_qag(double expected, double abserror,int key)
 	double result, error;
 	double alpha = 1.0;
 	gsl_function F;
+	double time = get_time();
 	printf("QAG GSL (blad %g, klucz %d): \n",abserror,key);
 	F.function = &f;
 	F.params = &alpha;
@@ -71,6 +84,7 @@ void integrate_gsl_qag(double expected, double abserror,int key)
 	printf ("estimated error = % .18f\n", error);
 	printf ("actual error    = % .18f\n", result - expected);
 	printf ("intervals       =  %d\n", w->size);
+	printf ("time            =  %f\n", get_time() - time);
 	gsl_integration_workspace_free (w);
 }
 
