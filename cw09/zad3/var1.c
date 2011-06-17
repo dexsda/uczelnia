f217021f18bb0dcb2485958d30a2ebd0dc5233d9
Karol Urbanski <dex@speeddemosarchive.com>
2011-06-17 13:06:06 +0200

    kurwwaaaaaaa

--- cw09/zad3/var1.c
+++ cw09/zad3/var1.c
@@ -6,6 +6,7 @@
 #include <time.h>
 #include "header.h"
 
+time_t wrtime=0,rdtime=0;
 int heap[MAX];
 int elements=0;
 pthread_mutex_t mutex_read = PTHREAD_MUTEX_INITIALIZER;
@@ -25,12 +26,15 @@ void * reader_thread(void * params)
 {
 	struct thread_params * args = params;
 	int i;
+	time_t t1,t2;
 	for(i=0; i<args->ops; i++){
+		t1=time(NULL);
 		pthread_mutex_lock(&mutex_read);
 		if(writer_in_da_house)
 			pthread_cond_wait(&cond_read,&mutex_read);
 		reader_dawgs++;
 		pthread_mutex_unlock(&mutex_read);
+		t2=time(NULL);
 		if(retmax(heap,elements)<0){
 			printf("reader %d: co to za biblioteka bez ksiazek?!!?\n",args->id);
 		} else {
@@ -42,6 +46,7 @@ void * reader_thread(void * params)
 		if(!reader_dawgs)
 			pthread_cond_broadcast(&cond_write);
 		pthread_mutex_unlock(&mutex_read);
+		rdtime+=t2-t1;
 	}
 	return NULL;
 }
@@ -50,11 +55,14 @@ void * writer_thread(void * params)
 {
 	struct thread_params * args = params;
 	int i;
+	time_t t1,t2;
 	for(i=0; i<args->ops; i++){
+		t1=time(NULL);
 		pthread_mutex_lock(&mutex_write);
 		if(reader_dawgs)
 			pthread_cond_wait(&cond_write,&mutex_write);
 		writer_in_da_house=1;
+		t2=time(NULL);
 		if(rand()%3){
 			int added=rand()%30 +1;
 			if(add(heap,elements,added)<0){
@@ -76,14 +84,15 @@ void * writer_thread(void * params)
 		writer_in_da_house=0;
 		pthread_cond_broadcast(&cond_read);
 		pthread_mutex_unlock(&mutex_write);
+		wrtime+=t2-t1;
 	}
 	return NULL;
 }
 
 int main(int argc, char * argv[])
 {
-	if(argc!=3){
-		fprintf(stderr,"%s [pisarze] [czytelnicy]\n",argv[0]);
+	if(argc!=7){
+		fprintf(stderr,"%s [czyt] [pis] [czyt_op] [czyt_sl] [pis_op] [pis_sl]\n",argv[0]);
 		exit(1);
 	}
 	int readers=atoi(argv[1]);
@@ -94,22 +103,28 @@ int main(int argc, char * argv[])
 	struct thread_params * writer_args = malloc(sizeof(struct thread_params)*writers);
 	int i;
 	srand(time(NULL));
+	int rops=atoi(argv[3]);
+	int rslp=atoi(argv[4]);
+	int wops=atoi(argv[5]);
+	int wslp=atoi(argv[6]);
 	for(i=0; i<readers; i++){
 		reader_args[i].id=i;
-		reader_args[i].sleeptime=rand()%2 + 1;
-		reader_args[i].ops=rand()%10+5;
+		reader_args[i].sleeptime=rslp;
+		reader_args[i].ops=rops;
 		pthread_create (&(reader_ids[i]),NULL, &reader_thread, &(reader_args[i]));
 	}
 
 	for(i=0; i<writers; i++){
 		writer_args[i].id=i;
-		writer_args[i].sleeptime=rand()%2 + 1;
-		writer_args[i].ops=rand()%10+5;
+		writer_args[i].sleeptime=wslp;
+		writer_args[i].ops=wops;
 		pthread_create (&(writer_ids[i]),NULL, &writer_thread, &(writer_args[i]));
 	}
 	for(i=0; i<readers; i++)
 		pthread_join(reader_ids[i],NULL);
 	for(i=0; i<writers; i++)
 		pthread_join(writer_ids[i],NULL);
+	printf("Przecietny czas oczekiwania na odczyt: %f \n",rdtime/(double)(rops*readers));
+	printf("Przecietny czas oczekiwania na zapis: %f \n",wrtime/(double)(wops*writers));
 	return 0;
 }
